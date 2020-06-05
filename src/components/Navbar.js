734b83b344773d0184c4c056e66ec3f1f7431f27
hyojelee <hyojelee1004ss@gmail.com>
2020-06-04 21:06:58 -0400

    add toggle and done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,87 +3,98 @@ import { Link } from "react-scroll";
 import { FaAlignRight } from "react-icons/fa";
 
 export default class Navbar extends Component {
-  // state = {
-  //   isOpen: false,
-  // };
+  state = {
+    isOpen: false,
+  };
 
-  // handleToggle = () => {
-  //   this.setState({ isOpen: !this.state.isOpen });
-  // };
-  // state = {
-  //   isOpen: false,
-  // };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  state = {
+    isOpen: false,
+  };
 
-  // handleToggle = () => {
-  //   this.setState({ isOpen: !this.state.isOpen });
-  // };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
   render() {
     return (
-      <nav className="nav">
-        <Link
-          className="logo"
-          to="home"
-          activeClass="active"
-          spy={true}
-          smooth={true}
-          offset={-300}
-          duration={500}
-        >
-          Hyoje
-        </Link>
-
-        <div className="navbar">
-          <ul className="nav-links show-nav">
-            <li>
-              <Link
-                to="home"
-                activeClass="active"
-                spy={true}
-                smooth={true}
-                offset={-300}
-                duration={500}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="project"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >
-                Project
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={500}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link
+              to="home"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-300}
+              duration={500}
+            >
+              HyoJe
+            </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon"></FaAlignRight>
+            </button>
+          </div>
+          <div className="nav-link">
+            <ul
+              className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+            >
+              <li className="nav-link">
+                <Link
+                  to="home"
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-300}
+                  duration={500}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-link">
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-link">
+                <Link
+                  activeClass="active"
+                  to="project"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                >
+                  Project
+                </Link>
+              </li>
+              <li className="nav-link">
+                <Link
+                  activeClass="active"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={500}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
